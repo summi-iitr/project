fb138ec3b59f27c256b43e64989d69c3dc5d81a8
Utkarsh <upandey@adobe.com>
2017-06-13 12:24:59 +0530

    no message

--- src/python/processors/query_processor.py
+++ src/python/processors/query_processor.py
@@ -18,13 +18,30 @@ def intention(ques):
        if any ( [ items[1] == 'WRB', items[1] == 'WP', items[1] == 'WDT'] ):
            return intent[items[1]][items[0].lower()]
 
+def nounword(ques):
+    words = word_tokenize(ques)
+    tagged = pos_tag(words)
 
+    grammar = r"""
+            NP:{<NN.*>}   #CHUNKING ALL THE NOUNS
+             """
+    cp = nltk.RegexpParser(grammar)
+    result = cp.parse(tagged)
+    featlist=[]
+    for subtree in result.subtrees():
+        if any( [subtree.label() == 'VP' , subtree.label() == 'NP'] ):
+            featlist.append( subtree.leaves()[0][0] )
+
+
+    return featlist
 
 
 
 question = read_input()
 pos_tags = process(question)
 qtype=intention(question)
-output_data(pos_tags)
-print qtype
+features=nounword(question)
+#output_data(pos_tags)
+#print qtype
+output_data(features)
 
