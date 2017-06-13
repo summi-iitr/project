7ba0a6154600efec38a685be9a449510dcb822ff
Utkarsh <upandey@adobe.com>
2017-06-13 12:25:58 +0530

    no message

--- src/python/processors/scraping.py
+++ src/python/processors/scraping.py
@@ -6,20 +6,22 @@ from io_utils import output_data
 from file_utils import absolute_path
 path = absolute_path('../../../samples')
 def scrape():
-    docmap={}
+    docmap=[]
 
     for filename in os.listdir(path):
         if not filename.endswith('.xml'):continue
         fullname = os.path.join(path,filename)
         tree = etree.parse(fullname)
         root = tree.getroot()
+        newElement={}
 
-        docmap[filename]={}
-        docmap[filename]['type']=root.tag
     
     
         for child in root:
-            docmap[filename][child.tag]=''.join(child.itertext()).replace('\n','')
+            newElement['filename']=filename
+            newElement['type']=child.tag
+            newElement['text']=''.join(child.itertext()).replace('\n','')
+            docmap.append(newElement)
 
 
     output_data(docmap)
