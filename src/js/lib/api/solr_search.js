f489e19bf51507165f96d2ba2f7a8e5a93a89701
Sumit Kumar <sukumar@adobe.com>
2017-06-13 12:18:31 +0530

    Added missing file

---
+++ src/js/lib/api/solr_search.js
@@ -0,0 +1,12 @@
+/*let $ = global.$
+
+module.exports = (callback, queryMap)=>{
+  $.ajax({
+    type: 'get',
+    url: "solr/search",
+    data:`q=${JSON.stringify(queryMap)}`,
+    success: callback
+  });
+}
+
+*/
