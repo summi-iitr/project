1fa576e3a140af825d2890b35ea0a585b390385d
Sumit Kumar <sukumar@adobe.com>
2019-12-02 10:17:12 +0530

    adding code to add project users

--- src/js/api/columns.js
+++ src/js/api/columns.js
@@ -13,12 +13,172 @@ module.exports = [
 
     },
     {
+        displayName: "Dept of PI",
+        name: "dept_pi",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Project Title",
+        name: "project_title",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "R&D",
+        name: "RD",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
         displayName: "Code",
         name: "code",
         type: "text",
         sqlType: "VARCHAR(255)"
 
-    }
+    },
+    {
+        displayName: "Dept",
+        name: "Dept",
+        type: "text",
+        sqlType: "VARCHAR(255)"
 
+    },
+    {
+        displayName: "Type of Scheme",
+        name: "type_scheme",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Project Code",
+        name: "code",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Year FY",
+        name: "Year_FY",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "No.",
+        name: "No",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "TYPE",
+        name: "type",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Sponsoring Agency",
+        name: "s_agency",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Govt./Pvt./Other",
+        name: "Govt_Pvt",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Wheat her industry funded",
+        name: "funded",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "File name",
+        name: "file_name",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Overhead",
+        name: "overhead",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Total Granted Fund",
+        name: "total_fund",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Duration",
+        name: "duration",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Sanction No",
+        name: "sanction_no",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Date of commencement",
+        name: "date_commencement",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Academic Year",
+        name: "academic_year",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Calander Year",
+        name: "calander_year",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Financial Year",
+        name: "financial_year",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Date of completion",
+        name: "Date_completion",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    },
+    {
+        displayName: "Status",
+        name: "status",
+        type: "text",
+        sqlType: "VARCHAR(255)"
+
+    }
 ]
 
